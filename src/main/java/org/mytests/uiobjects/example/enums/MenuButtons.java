a6dae83c45f0affe3ec0473b0b54da0107ed17fb
MIXSTAYS <32819223+MIXSTAYS@users.noreply.github.com>
2018-06-20 21:56:30 +0300

    added some new buttons

--- src/main/java/org/mytests/uiobjects/example/enums/MenuButtons.java
+++ src/main/java/org/mytests/uiobjects/example/enums/MenuButtons.java
@@ -7,11 +7,18 @@ import ru.yandex.qatools.allure.annotations.Description;
  */
 @Description("Buttons names from header menu")
 public enum MenuButtons {
-    HOME("Home"), CONTACT_FORM("Contact form"), SERVICE("Service"), METALS_AND_COLORS("Metals & Colors");
+    HOME("Home"), CONTACT_FORM("Contact form"), SERVICE("Service"), METALS_AND_COLORS("Metals & Colors"),
+    SUPPORT("Support"), DATES("Dates"), COMPLEX_TABLE("Complex Table "), SIMPLE_TABLE("Simple Table "),
+    TABLE_WITH_PAGES("Table with pages"), DIFFERENT_ELEMENTS("Different elements");
 
     public String value;
 
     MenuButtons(String value) {
         this.value = value;
+    }
+
+    @Override
+    public String toString() {
+        return value;
     }
 }
