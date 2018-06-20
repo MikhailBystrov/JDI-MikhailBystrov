90822656e850e631601bc536500ad295ecad6601
MIXSTAYS <32819223+MIXSTAYS@users.noreply.github.com>
2018-06-20 21:58:33 +0300

    renamed summ

--- src/main/java/org/mytests/uiobjects/example/site/pages/MetalsColorsPage.java
+++ src/main/java/org/mytests/uiobjects/example/site/pages/MetalsColorsPage.java
@@ -3,7 +3,7 @@ package org.mytests.uiobjects.example.site.pages;
 import com.epam.jdi.uitests.web.selenium.elements.pageobjects.annotations.JPage;
 import org.mytests.uiobjects.example.entities.MetalsColorsData;
 import org.mytests.uiobjects.example.site.forms.MetalsColorsForm;
-import org.mytests.uiobjects.example.site.forms.SummaryForm;
+import org.mytests.uiobjects.example.site.forms.SummForm;
 import org.mytests.uiobjects.example.site.sections.ResultSection;
 import ru.yandex.qatools.allure.annotations.Description;
 import ru.yandex.qatools.allure.annotations.Step;
@@ -17,13 +17,13 @@ import ru.yandex.qatools.allure.annotations.Step;
 @Description("'Metals and colors' page content")
 @JPage(url = "/metals-colors.html", title = "Metal and Colors")
 public class MetalsColorsPage extends CommonPage {
-    public SummaryForm summaryForm;
+    public SummForm summForm;
     public MetalsColorsForm metalsColorsForm;
     public ResultSection resultSection;
 
     @Step("Submit selected radios")
     public void submitForms(MetalsColorsData data) {
-        summaryForm.calculate(data);
+        summForm.calculate(data);
         metalsColorsForm.submit(data);
     }
 }
