3900b6ba46b23b7c3b9013fe976815c8c89a5e76
MIXSTAYS <32819223+MIXSTAYS@users.noreply.github.com>
2018-06-26 15:24:34 +0300

    edited annotations

--- src/test/java/org/mytests/tests/example/SimpleTestsInit.java
+++ src/test/java/org/mytests/tests/example/SimpleTestsInit.java
@@ -3,15 +3,22 @@ package org.mytests.tests.example;
 import com.epam.jdi.uitests.web.selenium.elements.composite.WebSite;
 import com.epam.jdi.uitests.web.testng.testRunner.TestNGBase;
 import org.mytests.uiobjects.example.site.JDIExampleSite;
-import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
 
 import static com.epam.jdi.uitests.core.settings.JDISettings.logger;
 
 @SuppressWarnings("unchecked")
 public class SimpleTestsInit extends TestNGBase {
-    @BeforeSuite(alwaysRun = true)
-    public static void setUp() {
+    @BeforeClass(alwaysRun = true)
+    public void setUp() {
+        System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
         WebSite.init(JDIExampleSite.class);
         logger.info("Run Tests");
+//        JDIFrameworkSite.indexPage.open();
+    }
+    @AfterMethod(alwaysRun = true)
+    public void refreshPage() {
+        JDIExampleSite.metalsColorsPage.headerSection.logout();
     }
 }
