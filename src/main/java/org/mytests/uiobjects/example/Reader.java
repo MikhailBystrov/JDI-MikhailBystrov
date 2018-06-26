401a10ae0110fe84d1ac657dd0440a8374941209
MIXSTAYS <32819223+MIXSTAYS@users.noreply.github.com>
2018-06-26 15:25:22 +0300

    created json reader

---
+++ src/main/java/org/mytests/uiobjects/example/Reader.java
@@ -0,0 +1,30 @@
+package org.mytests.uiobjects.example;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import com.google.gson.stream.JsonReader;
+import org.mytests.uiobjects.example.entities.MetalsColorsData;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.lang.reflect.Type;
+import java.util.Map;
+
+/**
+ * Created by Mikhail on 25.06.2018
+ */
+public class Reader {
+    private static Map<String, MetalsColorsData> dataMap = null;
+    private static File file = new File("src\\test\\resources\\JDI_ex8_metalsColorsDataSet.json");
+
+    public static Map<String, MetalsColorsData> readFile() {
+        try (JsonReader jsonReader = new JsonReader(new FileReader(file))) {
+            Type token = new TypeToken<Map<String, MetalsColorsData>>() {{}}.getType();
+            dataMap = new Gson().fromJson(jsonReader, token);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return dataMap;
+    }
+}
